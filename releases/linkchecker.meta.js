4e37dfb29735a9abc4c5900f8a6cc376156141df
XOR <xor10@users.noreply.github.com>
2018-05-15 13:00:35 -0300

    Atualizado repositório

---
+++ releases/linkchecker.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Verificador de uploads [FW]
+// @description Userscript para auxiliar na verificação de uploads na FileWarez.
+// @include     /^http(s)?:\/\/(www\.)?filewarez\.tv/.*$/
+// @copyright   2016, XOR
+// @author      XOR
+// @version     0.3.3
+// @grant       unsafeWindow
+// @grant       GM_xmlhttpRequest
+// @require     https://linkcheckerfw.github.io/lib/asmcrypto.js
+// @updateURL   https://linkcheckerfw.github.io/releases/linkchecker.meta.js
+// @downloadURL https://linkcheckerfw.github.io/releases/linkchecker.user.js
+// @namespace   4971e63b909612f5598edd6d90012dae
+// @connect     *
+// @run-at      document-start
+// @noframes
+// ==/UserScript==
